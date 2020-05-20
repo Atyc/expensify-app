ab0368f7d49b364e7d33fa1e8f0b47b91e5dc0ad
Octav@home <contact@octav.co.uk>
2020-05-20 14:46:31 +0100

    Setup production webpack

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
@@ -19,13 +18,10 @@ store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const visibleExpenses = getVisibleExpenses(store.getState().expenses, store.getState().filters);
 
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
-
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
